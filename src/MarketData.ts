18f276f2d734f174df62a728ab9593c355a72076
mrudangit <mrudang@yahoo.com>
2020-02-24 08:15:44 -0500

    Amps

---
+++ src/MarketData.ts
@@ -0,0 +1,8 @@
+export class MarketData {
+
+    public Cusip:string;
+    public Price:number;
+    public Yield:number;
+    public BenchMarkCusip:string;
+
+}
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,60 @@
 console.log('Hello Node TypeScript');
+import {Client} from 'amps';
+import {MarketData} from './MarketData';
+console.log('Amps Client Version : ', Client.version());
+
+
+const ampsClient = new Client('TraderWorkStationClient');
+
+ampsClient.connect('ws://mrudang-WS-Z390-PRO:9010/amps/json').then(value => {
+    console.log('Connected...');
+    const md = new MarketData();
+    md.Cusip = '10Y';
+    md.Price = 100.12;
+    md.Yield = 3;
+    console.log('Publishing Market Data : ', md);
+    ampsClient.publish('TraderWorkstation/BenchMarkMarketData', md);
+});
+
+setInterval(()=> {
+    publishBenchMarketData('10Y');
+},5000);
+
+
+setInterval(()=> {
+    publishBondMarketData('IBM');
+    publishBondMarketData('AAPL');
+
+    publishBondMarketDataInternal('IBM');
+    publishBondMarketDataInternal('AAPL');
+},1000);
+
+
+function publishBondMarketData(symbol: string) {
+    const md = new MarketData();
+    md.Cusip = symbol;
+    md.Price = 200 + Math.random();
+    md.Yield = 4 + Math.random();
+    md.BenchMarkCusip = '10Y';
+    ampsClient.publish('TraderWorkstation/BondMarketData', md);
+}
+
+
+
+function publishBenchMarketData(symbol: string) {
+    const md = new MarketData();
+    md.Cusip = symbol;
+    md.Price = 100.12;
+    md.Yield = 3 + Math.random();
+    ampsClient.publish('TraderWorkstation/BenchMarkMarketData', md);
+}
+
+
+function publishBondMarketDataInternal(symbol: string) {
+    const md = new MarketData();
+    md.Cusip = symbol;
+    md.Price = 200 + Math.random();
+    md.Yield = 4 + Math.random();
+    md.BenchMarkCusip = '10Y';
+    ampsClient.publish('TraderWorkstation/BondMarketDataInternal', md);
+}
